eaccb4e3d29699658b8634cecd308e51b029874f
Jonathan <jonathan.vieira@yahoo.com.br>
2022-01-30 22:05:21 -0300

    a

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 import streamlit as st
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from pycaret.classification import load_model, predict_model
 import plotly.express as px
 import acessando_blob_storage as abs
@@ -17,16 +17,18 @@ st.set_page_config(
 
 #Carregar Bases
 abs.download_blob('presentation', 'final_model.pkl', 'final_model.pkl') # Modelo de Classificação
-abs.download_blob('presentation', 'sellers_in_out.csv', 'sellers_in_out.csv') # Tabela de entradas e saídas 
+abs.download_blob('presentation', 'sellers_in_out4.csv', 'sellers_in_out4.csv') # Tabela de entradas e saídas 
 abs.download_blob('presentation', 'df_cluster.csv', 'df_cluster.csv') # Tabela Cluster 
 abs.download_blob('presentation', 'tb_base.csv', 'tb_base.csv') # Tabela que alimenta modelo
 abs.download_blob('presentation', 'banner.jpeg', 'banner.jpeg') # Banner do App
+abs.download_blob('presentation', 'retention.csv', 'retention.csv') # Banner do App
 
 var_model = "final_model"
 
 var_dataset_modelo = "tb_base.csv"
-var_sellers_in_out= "sellers_in_out.csv"
+var_sellers_in_out= "sellers_in_out4.csv"
 var_cluster="df_cluster.csv"
+var_retention="retention.csv"
 
 #carregando o modelo treinado.
 model = load_model(var_model)
@@ -40,8 +42,10 @@ dataset_modelo= dataset_modelo.drop(dataset_modelo.columns[0], axis=1)
 
 
 dataset_cluster= pd.read_csv(var_cluster)
+dataset_sellers=pd.read_csv(var_sellers_in_out)
+dataset_retention=pd.read_csv(var_retention, index_col=0)
 
-print (dataset_modelo.head())
+#print (dataset_modelo.head())
 
 #st.image("banner.jpeg", width=100)
 st.image("banner.jpeg")
@@ -53,14 +57,27 @@ st.title("Olist Analytics")
 st.markdown("Este é um Data App utilizado para exibir a solução de Machine Learning para Olist Analytics.")
 
 # imprime o conjunto de dados usado
-st.dataframe(dataset_modelo.head())
+#st.dataframe(dataset_modelo.head())
 
-
-#st.line_chart(data=var_sellers_in_out, use_container_width=True)
-
-
-fig= px.scatter_3d(dataset_cluster,x='media_produtos_por_pedido',y='media_valor_pedido_sem_frete',z='dias_atividade',color='cluster')
+fig = px.line(dataset_sellers, x="data_pedido", y=["sellers_in", "sellers_out"], title='Sellers In x Out')
 st.plotly_chart(fig, use_container_width=True)
+
+fig2 = px.histogram(dataset_modelo, x="dias_atividade")
+st.plotly_chart(fig2, use_container_width=True)
+
+
+
+fig6, ax = plt.subplots(figsize=(17,8))
+sns.heatmap(dataset_retention, ax=ax, annot=True, fmt = '.0%', cmap='summer_r')
+plt.style.use('fivethirtyeight')
+plt.title('Retention Rates')
+st.pyplot(fig6)
+
+
+
+fig4= px.scatter_3d(dataset_cluster,x='media_produtos_por_pedido',y='media_valor_pedido_sem_frete',z='dias_atividade',color='cluster')
+
+st.plotly_chart(fig4, use_container_width=True)
 
 #st.subheader("Defina os atributos do empregado para predição de turnover")
 

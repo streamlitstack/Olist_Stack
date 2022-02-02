f1903f04bf5a78586c0c1b8a50f84ec7ec153359
Jonathan <jonathan.vieira@yahoo.com.br>
2022-02-02 10:40:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ plt.style.use('fivethirtyeight')
 plt.ylabel('Cohort Group', fontsize = 15) # y-axis label with fontsize 15
 plt.xlabel('Cohort Period', fontsize = 15) # y-axis label with fontsize 15
 plt.title('Cohort Analysis (%) - Retention Rates', fontsize=20)
-plt.figure(facecolor='yellow')
+ax.set_facecolor('pink')
 st.pyplot(fig2)
 
 st.markdown("""---""")
